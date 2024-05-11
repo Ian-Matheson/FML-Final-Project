63f5c4ba5380472fa9ee01d60d50ddfd0dbd9fb1
Zeb Becker <zbecker2@bowdoin.edu>
2024-05-11 12:08:34 -0400

    Add simple ID col to tank data

--- FRT_coords/extract_data.py
+++ FRT_coords/extract_data.py
@@ -6,23 +6,26 @@ import json
 import csv
 
 # read in json 
-with open("FRT_coords/tank_inventory.json", "r") as file:
+with open("./tank_inventory.json", "r") as file:
     json_data = file.read()
 data = json.loads(json_data)
 
 # write data to csv
-with open("FRT_coords/tank_inventory.csv", "w", newline='') as csv_file:
+with open("./tank_inventory.csv", "w", newline='') as csv_file:
     writer = csv.writer(csv_file)
 
-    writer.writerow(['Tanker Type', 'Tile', 'NW Coor (Long, Lat)', 'NE Coor (Long, Lat)', 
+    writer.writerow(['ID', 'Tanker Type', 'Tile', 'NW Coor (Long, Lat)', 'NE Coor (Long, Lat)', 
                      'SE Coor (Long, Lat)', 'SW Coor (Long, Lat)', 'Capture Date', 'County', 'State', "Coords"])
 
+    current_id = 0
     for feature in data['features']:
         properties = feature['properties']
         geometry = feature['geometry']
 
         tanker_type = properties['object_class']
         if tanker_type == "external_floating_roof_tank":
+            id = current_id
+            current_id += 1
             tile = properties['tile_name']
             capture_date = properties['capture_date']
             county = properties['county']
@@ -35,6 +38,6 @@ with open("FRT_coords/tank_inventory.csv", "w", newline='') as csv_file:
             se_coor = tuple(all_coords[2])
             ne_coor = tuple(all_coords[3])
 
-            writer.writerow([tanker_type, tile, nw_coor, ne_coor, se_coor, sw_coor, capture_date, 
+            writer.writerow([id, tanker_type, tile, nw_coor, ne_coor, se_coor, sw_coor, capture_date, 
                              county, state, all_coords])
 
